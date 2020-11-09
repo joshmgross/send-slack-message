a314c9074cf047757f78f9045643074ca07d3a1a
Josh Gross <joshmgross@github.com>
2020-11-09 17:09:48 -0500

    Create send-slack-action

---
+++ README.md
@@ -0,0 +1,27 @@
+# Send Slack Message
+
+This is a GitHub action to send a Slack message using a Slack bot token.
+
+## Set up
+
+See https://api.slack.com/bot-users for information on setting up and installing a Slack Bot.
+
+## Inputs
+
+- `message`: The Slack message to send
+
+- `slack-token`: The Slack bot user access token
+
+- `channel`: The Slack channel ID to send the message to
+
+## Example Usage
+
+
+```yaml
+      - name: Post message
+        uses: joshmgross/send-slack-message@main
+        with:
+          message: 'Hello world'
+          channel: ${{ secrets.SLACK_CHANNEL_ID }}
+          slack-token: ${{ secrets.SLACK_BOT_TOKEN }}
+```
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { WebClient } from '@slack/web-api';
+import * as core from "@actions/core";
+
+async function run(): Promise<void> {
+    try {
+        // Inputs and validation
+        const slackToken = core.getInput("slack-token", { required: true });
+        const channel = core.getInput("channel", { required: true });
+        const message = core.getInput("message", { required: true });
+
+        // Send slack message
+        const slackWebClient =  new WebClient(slackToken);
+        const result = await slackWebClient.chat.postMessage({
+          text: message,
+          channel: channel
+        });
+
+        if (result.ok) {
+          core.info('Slack message sent 🚀')
+        } else {
+          core.setFailed(`❌ Unable to send Slack message: ${result.error}`);
+        }
+
+    } catch (error) {
+      core.setFailed(`❌ Action failed with error: ${error}`)
+    }
+}
+
+run();
